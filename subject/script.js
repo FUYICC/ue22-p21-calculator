4866a0651b8b4f58e70d330785bb567cd98d7312
FUYICC <yunmeng.shu@mines-paristech.fr>
2022-02-16 16:30:04 +0100

    Update script.js

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,21 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+window.addEventListener("load", () =>{
+    let buttons = document.getElementByTagName('button');
+    for (let button of buttons){
+        button.addEventListener("click", (event) =>
+            {console.log(event.target.textContent);
+        })
+        //也可以用button.onclick = () => ...
+    }
+    // 到这一步我们实现了点一个按钮，在console显示点的多少
+    // button.addEventListener("click", (event) =>
+    //    {console.log(event.target.textContent);
+    //是指我们创建了一个function来实现打出这个event，可以写成：
+    // 先定义function printTargetText(event){
+    //          console.log(event.target.textContent}
+    //然后  button.addEventListener("click", printTargetText;
+}
+
+)
